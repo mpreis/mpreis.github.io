970c98dfe9fc4e8cf0883082524dbaa48de7a41b
Mario Preishuber <mario.preishuber@stud.sbg.ac.at>
2017-01-31 15:18:33 +0100

    Update.

--- assets/data/makepubs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Call from base directory:
-#   _contrib/makepubs.sh
-
-
-TMPDIR=. bibtex2html -nobibsource -noheader -nofooter -nodoc -unicode -dl -nokeys -d -r mp.bib
-
-cp mp.bib mp.bib.txt
